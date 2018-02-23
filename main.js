f9177fa08de1a8c9dfe0653922a9ea48024a49b7
Paul Uncangco <pju93@hotmail.com>
2018-02-23 18:21:59 -0500

    Get lat & lng and convert it to address with xmlhttprequest

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
-let map, infoWindow;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-    });
-    infoWindow = new google.maps.InfoWindow;
+function getLocation(latitude,longitude){
+  var request = new XMLHttpRequest();
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+  var method = 'GET';
+  var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
+  var async = true;
 
-            //   console.log(pos);
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        //   Borwser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+  request.open(method, url, async);
+  request.onreadystatechange = function(){
+    if(request.readyState == 4 && request.status == 200){
+      var data = JSON.parse(request.responseText);
+      var address = data.results[2];
+      document.write(address.formatted_address);
+      console.log(address);
     }
-}
+  };
+  request.send();
+};
 
-function handleLocationError(browserHasGeoLocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeoLocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation');
-    infoWindow.open(map);
-}
+var successCallback = function(position){
+  var x = position.coords.latitude;
+  var y = position.coords.longitude;
+  getLocation(lat,lng);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback);
