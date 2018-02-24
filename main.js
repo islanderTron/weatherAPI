85529a84eef0002530f2c463e6cfaf8b392118ac
Paul Uncangco <pju93@hotmail.com>
2018-02-23 21:24:17 -0500

    Weather Forecast - in progress

--- main.js
+++ main.js
@@ -2,25 +2,41 @@ function getLocation(latitude,longitude){
   var request = new XMLHttpRequest();
 
   var method = 'GET';
-  var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
+  var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude +"&key=AIzaSyAYSzN7cCjKN9WE5YqrYyv-ckUv-l-D95k";
   var async = true;
 
   request.open(method, url, async);
   request.onreadystatechange = function(){
     if(request.readyState == 4 && request.status == 200){
       var data = JSON.parse(request.responseText);
-      var address = data.results[2];
-      document.write(address.formatted_address);
-      console.log(address);
+      console.log(data);
+      var address = data.results[5];
+
+      // Set a current for state (2 letters) & city and pass it to functions at below.
+      var currentCity = address.address_components[1].long_name;
+      var currentState = address.address_components[2].short_name;
+      currentForeCast(currentCity, currentState);
+      console.log(currentCity + "," + currentState);
     }
   };
   request.send();
 };
 
 var successCallback = function(position){
-  var x = position.coords.latitude;
-  var y = position.coords.longitude;
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
   getLocation(lat,lng);
 };
 
 navigator.geolocation.getCurrentPosition(successCallback);
+
+// Need to set up abstract json request function for variety functionality for weather.  
+function currentForeCast(city, state) {
+  let method = "GET", async = true, url = "http://api.wunderground.com/api/122e30171af0a6c6/forecast/q/" + state + "/" + city +".json";
+  request.open(method, url, async);
+  request.onreadystatechange = function() {
+    if(request.readyState == 4 && request.status == 200) {
+
+    }
+  }
+}
